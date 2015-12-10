8adb087de226a3ed3e6309036ea3def70e34627b
JieYing Peng <jipeng@syr.edu>
2015-12-09 19:02:40 -0800

    fixed conflicts

--- client/app/app.js
+++ client/app/app.js
@@ -1,21 +1,9 @@
 'use strict';
 
 angular.module('bApp', [
-<<<<<<< HEAD
-    'bApp.auth',
-    'bApp.admin',
-    'bApp.geolocation',
-    // 'bApp.campaignProfile',
-    'bApp.constants',
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ui.router',
-    'validation.match'
-  ])
-=======
   'bApp.auth',
   'bApp.admin',
+  'bApp.geolocation',
   // 'bApp.campaignProfile',
   'bApp.constants',
   'ngCookies',
@@ -24,10 +12,10 @@ angular.module('bApp', [
   'ui.router',
   'validation.match'
 ])
->>>>>>> 96853e0054fbd533474303c8a802260a6aab9549
-  .config(function($urlRouterProvider, $locationProvider) {
-    $urlRouterProvider
-      .otherwise('/');
 
-    $locationProvider.html5Mode(true);
-  });
+.config(function($urlRouterProvider, $locationProvider) {
+  $urlRouterProvider
+    .otherwise('/');
+
+  $locationProvider.html5Mode(true);
+});
