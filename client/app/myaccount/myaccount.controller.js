f770f7d9dc9c4842ac45463ad039dc90a95a95c4
JieYing Peng <jipeng@syr.edu>
2015-12-15 13:01:44 -0800

    fixed css

--- client/app/myaccount/myaccount.controller.js
+++ client/app/myaccount/myaccount.controller.js
@@ -4,9 +4,36 @@ angular.module('bApp')
   .controller('MyaccountCtrl', ['$scope', 'Auth', function($scope, Auth) {
     $scope.getCurrentUser = Auth.getCurrentUser;
     $scope.name = $scope.getCurrentUser().name;
-       $scope.profile_pic = $scope.getCurrentUser().profile_pic;
+    $scope.profile_pic = $scope.getCurrentUser().profile_pic;
     $scope.click = function() {
       console.log($scope.getCurrentUser()._id);
     };
 
+ $scope.filters = [
+            {
+                'filterId': 1,
+                'time': 'Followed',
+                'url': '/following'
+            },
+            {
+                'filterId': 2,
+                'time': 'Started',
+                'url': '/myaccount'
+            },
+            {
+                'filterId': 3,
+                'time': 'Contributed',
+                'url': '/contributed'
+            }
+        
+        ];
+    $scope.selected = 1;
+
+    $scope.select= function(index) {
+    	console.log(index);
+       $scope.selected = index; 
+    };
+
   }]);
+
+
