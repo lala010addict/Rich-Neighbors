51486c16cc806cd78d29973edf78525bf2cc824f
Abdul-Rahman Taleb-Agha <abdulrahman@talebagha.net>
2016-01-04 21:01:32 -0800

    moved directives to component folder, cleaned up MainController using ES6 Classes

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,176 +1,86 @@
 'use strict';
 
-angular.module('bApp.MainController', ['ui.router'])
-  .controller('MainController', ['$scope', '$http', 'geolocationFactory', function ($scope, $http, geolocationFactory) {
+(function () {
 
-    $scope.campaigns = {};
+class MainController {
+  constructor($http, geolocationFactory) {
+    var _this = this;
+    //this.scope = $scope;
+    this.http = $http;
+    this.campaigns = {};
+    this.outputBar = {
+      bar: 'main'
+    };
+    this.offsetLevel = 1;
+    this.getCurrentLoc()
+      .then(() => {
+        _this.addMoreResults(500);
+      });
+  };
 
-    // https://maps.googleapis.com/maps/api/distancematrix/json?origins=02148&destinations=91801
-    $scope.location = geolocationFactory.getLatandLong();
-
-    $scope.getCurrentLoc = function() {
+  getCurrentLoc() {
+    var _this = this;
     var url = 'http://ipinfo.io/json';
-      return $http.get(url)
-      .success(function(data) {
-        var result = data.city + ' ' + data.region + ', ' + data.postal;
-        $scope.currentLoc = result;
-        $scope.loc = data.loc.split(',').map(function(loc) {
-          console.log(loc);
+    return _this.http.get(url)
+      .success(data => {
+        _this.currentLoc = `${data.city}, ${data.region} ${data.postal}`;
+        console.log(_this.currentLoc);
+        _this.loc = data.loc.split(',').map(function(loc) {
           return Number(loc);
         });
       });
-
-    };
-    $scope.currentLoc = 'Me';
-    $scope.outputBar = {bar : "main"};
-    $scope.offsetLevel = 1;
-
-    $scope.addMoreResults = function (dist) {
-      var distance = dist || 500;
-      var limit = 18 + ($scope.ofsetLevel * 9);
-      $http({
+  };
+  addMoreResults(dist) {
+    var _this = this;
+    var distance = dist || 500;
+    var limit = 18 + _this.offsetLevel * 9;
+    this.http({
         method: 'GET',
         url: '/api/campaigns',
-        params: { longitude: $scope.loc[0] , latitude: $scope.loc[1] , limit: limit, distance: distance}
+        params: {
+          longitude: this.loc[0],
+          latitude: this.loc[1],
+          limit: limit,
+          distance: distance
+        }
       })
-      .success(function(data) {
-        $scope.campaigns = data //_.extend($scope.campaigns, data);
-        console.log(data);
-        $scope.offsetLevel += 1;
+      .success(data => {
+        _this.campaigns = data //_.extend($campaigns, data);
+        _this.offsetLevel += 1;
       })
-      .error(function(data) {
-        console.log('Error: ' + data);
+      .error(err => {
+        console.error('Error: ' + err);
       });
-    }
-
-    $scope.getCurrentLoc()
-      .then(function () {
-        $scope.addMoreResults(500);
-      });
-
-
-
-
-    $scope.calDonatedAmount = function(x) {
-      var amounts = _.pluck(x, 'amount');
-        // console.log(amounts)
-
-      return _.reduce(amounts, function(total, n) {
-        return total + n;
-      });
+  };
+  calDonatedAmount(x) {
+    var amounts = _.pluck(x, 'amount');
+    return _.reduce(amounts, function(total, n) {
+      return total + n;
+    });
+  };
+  limitChar(x, y) {
+    var sp = x.split('');
+    return sp.slice(0, y).join('');
+  };
+  showData() {
+    var self = this;
+    var pagesShown = 1;
+    var pageSize = 9;
+    this.paginationLimit = function() {
+      return pageSize * pagesShown;
     };
-
-    $scope.limitChar = function(x, y) {
-      var sp = x.split('');
-      return sp.slice(0, y).join('');
+    this.hasMoreItemsToShow = function() {
+      return pagesShown < (self.campaigns.length / pageSize);
     };
-
-    // $scope.showCampaignProfile = function(x) {
-    //   $location.path('#/campaignProfile/' + x._id);
-    // };
-
-
-    $scope.showData = function() {
-
-      //show more functionality
-      var pagesShown = 1;
-      var pageSize = 9;
-
-      $scope.paginationLimit = function() {
-        return pageSize * pagesShown;
-      };
-      $scope.hasMoreItemsToShow = function() {
-        return pagesShown < ($scope.campaigns.length / pageSize);
-      };
-      $scope.showMoreItems = function() {
-        pagesShown = pagesShown + 1;
-        $scope.addMoreResults();
-      };
-
-
+    this.showMoreItems = function() {
+      pagesShown = pagesShown + 1;
+      self.addMoreResults();
     };
+  }
+}
 
+//MainController.$inject = ['$scope', '$http', 'geolocationFactory'];
 
-
-  }])
-  .directive('heroCard', function () {
-    return {
-      templateUrl: 'app/main/herocard.html',
-      restrict: 'EA',
-      link: function (scope, element, attrs) {
-      }
-    };
-  })
-  .directive('campaignCard', function () {
-    return {
-      templateUrl: 'app/main/temp.html',
-      restrict: 'EA',
-      link: function (scope, element, attrs) {
-      }
-    };
-  })
-  .directive('howCard', function () {
-    return {
-      templateUrl: 'app/main/howitworks.html',
-      restrict: 'EA',
-      link: function (scope, element, attrs) {
-      }
-    };
-  })
-  .directive('androidDemo', function () {
-    return {
-      templateUrl: 'app/main/androiddemo.html',
-      restrict: 'EA',
-      link: function (scope, element, attrs) {
-      }
-    };
-  })
-// .filter('zipCodeFilter', function(){
-//   return function(input){
-//     var out = [];
-//     angular.forEach(input, function(campaign){
-//       var zip = campaign.address.zip;
-//       if(zip[0] === $scope.zipcode[0]){
-//         out.push(campaign);
-//       }
-//     });
-//     return out;
-//   };
-// });
-
-
-
-
-
-
-// (function() {
-
-// class MainController {
-
-
-
-//   constructor($http) {
-//     this.$http = $http;
-//     this.awesomeThings = [];
-
-//     $http.get('/api/things').then(response => {
-//       this.awesomeThings = response.data;
-//     });
-//   }
-
-//   addThing() {
-//     if (this.newThing) {
-//       this.$http.post('/api/things', { name: this.newThing });
-//       this.newThing = '';
-//     }
-//   }
-
-//   deleteThing(thing) {
-//     this.$http.delete('/api/things/' + thing._id);
-//   }
-// }
-
-// angular.module('bApp')
-//   .controller('MainController', MainController);
-
-// })();
+angular.module('bApp.MainController', ['ui.router'])
+  .controller('MainController', MainController);
+})();
